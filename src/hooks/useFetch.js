dfb00884aab4e1ac3b026a3c9046b9efdef01c0d
shivamu1818 <shivamuoadhyay4@gmail.com>
2026-05-14 11:24:12 +0530

    added useFetch custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const useFetch = (api, query = "") => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    callApi(api, query);
+  }, [api, query]);
+  const callApi = async (api, query) => {
+    const url = query ? `${api}?query=${encodeURIComponent(query)}` : api;
+    setError(null);
+    setIsLoading(true);
+    try {
+      const data = await fetch(url);
+      const responseData = await data.json();
+      setData(responseData?.data ?? []);
+    } catch (err) {
+      setError(err.response);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return { isLoading, error, data };
+};
+
+export default useFetch;
